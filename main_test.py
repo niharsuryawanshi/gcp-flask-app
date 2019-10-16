f5127a3c2a8f6e929767a62f6679afcb6a05294d
Nihar Suryawanshi <nihar.suryawanshi@gmail.com>
2019-10-16 00:09:52 -0500

    additional

--- main_test.py
+++ main_test.py
@@ -1,2 +1,11 @@
-import flask
-print("hello")
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
+
